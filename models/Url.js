968b3aab3395f9213cff7dbde236cdfb0e1e9191
Dipak Dushing <97525751+dipakdushing@users.noreply.github.com>
2022-05-27 08:38:31 +0530

    Update Url.js
    Upadated

--- models/Url.js
+++ models/Url.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema({
-  urlCode: String,
-  longUrl: String,
-  shortUrl: String,
+const urlSchema = new mongoose.Schema ({
+  urlCode : String,
+  longUrl : String,
+  shortUrl : String,
   date: { type: Date, default: Date.now },
 });
 
