fcf1813c833b5cbf0ee02f61c68f43290f7bab00
Dipak Dushing <97525751+dipakdushing@users.noreply.github.com>
2022-05-27 08:39:43 +0530

    Update index.js
    Upadated

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,17 @@ const router = express.Router();
 
 const Url = require("../models/Url");
 
-// route GET /:code
-// @desc redirect to longUrl
-
 router.get("/:code", async (req, res) => {
   try {
     const url = await Url.findOne({ urlCode: req.params.code });
     if (url) {
       return res.redirect(url.longUrl);
     } else {
-      return res.status(404).json("No Url found");
+      return res.status(404).json("No URL found");
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json("Server Error");
+    res.status(500).json("Server Error Occured");
   }
 });
 
